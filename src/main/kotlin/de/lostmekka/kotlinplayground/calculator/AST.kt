2dcd0eac0eda32c1fb7c4e35d74bb80c43e12ff0
Allison Beckstrom <Slugamoon@gmail.com>
2018-08-16 18:56:37 -0500

    Add evaluation for ASTs

--- src/main/kotlin/de/lostmekka/kotlinplayground/calculator/AST.kt
+++ src/main/kotlin/de/lostmekka/kotlinplayground/calculator/AST.kt
@@ -1,5 +1,7 @@
 package de.lostmekka.kotlinplayground.calculator
 
+import de.lostmekka.kotlinplayground.calculator.Operator.*
+
 sealed class ASTNode {
     abstract fun evaluate(): Double
 }
@@ -13,18 +15,30 @@ enum class Operator {
 
 class BinaryOperator(val op: Operator, val left: ASTNode, val right: ASTNode) : ASTNode() {
     override fun evaluate(): Double {
-        TODO()
+        val l = left.evaluate()
+        val r = right.evaluate()
+        return when (op) {
+            Add -> l + r
+            Mul -> l * r
+            Div -> if (r == 0.0) throw EvaluateException("it is a division by zero")
+            else return l / r
+            Sub -> return l - r
+        }
     }
 }
 
 class UnaryOperator(val op: Operator, val value: ASTNode) : ASTNode() {
     override fun evaluate(): Double {
-        TODO()
+        val v = value.evaluate()
+        return when (op) {
+            Sub -> -v
+            else -> throw EvaluateException("$op is an invalid unary operator")
+        }
     }
 }
 
 class Value(val value: Double) : ASTNode() {
     override fun evaluate(): Double {
-        TODO()
+        return value
     }
 }
